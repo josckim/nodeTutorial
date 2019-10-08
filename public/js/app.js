f64bea0dc1063536d3f760072c6c77936cf07f12
Joscelyn Kim <joscelynkim@Joscelyns-MacBook-Pro.local>
2019-10-08 14:09:37 -0700

    init project setup

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+console.log('Client side javascript file is loaded')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    const location = search.value
+    console.log(location)
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = '...'
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = 'ERROR'
+                messageTwo.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
